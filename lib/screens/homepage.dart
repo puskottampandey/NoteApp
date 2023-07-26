f945ba76ea3113500a3323b0fffd6af4a3c46ecf
puskottampandey <puskottam.pandey@gmail.com>
2023-07-26 10:36:58 +0545

    search bar added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -13,8 +13,64 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundcolor,
-      body: Column(
-        children: const [Text("Notes")],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 100, 20, 10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Notes",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade800,
+                        borderRadius: BorderRadiusDirectional.circular(10)),
+                    child: const Icon(
+                      Icons.sort,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    hintText: "Search notes",
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    fillColor: (Colors.grey.shade800),
+                    filled: true,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey.shade800,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.grey,
+        ),
       ),
     );
   }

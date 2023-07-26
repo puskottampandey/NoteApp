03b2c387c084b353439cb82a2b7c92ac2f157d8e
puskottampandey <puskottam.pandey@gmail.com>
2023-07-26 11:23:14 +0545

    listview added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -13,55 +13,72 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundcolor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 100, 20, 10),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Notes",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade800,
-                        borderRadius: BorderRadiusDirectional.circular(10)),
-                    child: const Icon(
-                      Icons.sort,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 100, 20, 10),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Notes",
+                  style: TextStyle(
                       color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade800,
+                      borderRadius: BorderRadiusDirectional.circular(10)),
+                  child: const Icon(
+                    Icons.sort,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  hintText: "Search notes",
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  fillColor: (Colors.grey.shade800),
+                  filled: true,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.grey.shade800,
+                    child: const ListTile(
+                      leading: Text(
+                        "puskottam",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
                     ),
-                  )
-                ],
+                  );
+                },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    hintText: "Search notes",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    fillColor: (Colors.grey.shade800),
-                    filled: true,
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    )),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
